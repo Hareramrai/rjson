49d7a7ed52030dd515f6ab2015529656608f59eb
Hare Ram Rai <guru.java.ram@gmail.com>
2020-02-03 18:00:10 +0530

    WIP for deprecation warnings for mime type

--- lib/rjson/handler.rb
+++ lib/rjson/handler.rb
@@ -4,15 +4,15 @@ require 'rjson/string'
 module RJSON
   class Handler
     class_attribute :default_format
-    self.default_format = Mime::JSON
-    
+    self.default_format = Mime[:json]
+
     class << self
       def call(template)
         <<-COMPILED_METHOD
           contents=begin;
             #{template.source}
           end
-        
+
           RJSON::String.new(contents.to_json)
         COMPILED_METHOD
       end
